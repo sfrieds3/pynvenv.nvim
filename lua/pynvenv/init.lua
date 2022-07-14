6e99cf807b89b1fb86d5707dc2396527fefff239
scwfri <scwfri@gmail.com>
2022-07-14 16:16:53 -0500

    Do not set virtualenv if one is already set, set VIRTUAL_ENV

--- lua/pynvenv/init.lua
+++ lua/pynvenv/init.lua
@@ -4,8 +4,12 @@ function M.setup(opts)
   require('pynvenv.config').setup(opts)
 end
 
+local function get_venv_path(venv)
+  return require('pynvenv.config').aliases(venv)
+end
+
 local function get_venv_bin(venv)
-  local venv_dir = require('pynvenv.config').aliases(venv)
+  local venv_dir = get_venv_path(venv)
   return string.format('%s/bin', venv_dir)
 end
 
@@ -20,12 +24,19 @@ function M.deactivate()
     vim.env.PYTHONHOME = vim.env.OLD_VIRTUAL_PYTHONHOME
     vim.env.OLD_VIRTUAL_PYTHONHOME = nil
   end
+
+  vim.env.VIRTUAL_ENV = nil
 end
 
 function M.activate(venv)
+  if vim.env.VIRTUAL_ENV then
+    return
+  end
+  local venv_path = get_venv_path(venv)
   local venv_bin = get_venv_bin(venv)
 
   M.deactivate()
+  vim.env.VIRTUAL_ENV = venv_path
   vim.env.OLD_VIRTUAL_PATH = vim.env.PATH
   vim.env.PATH = venv_bin .. ':' .. vim.env.PATH
   if vim.env.PYTHONHOME then
